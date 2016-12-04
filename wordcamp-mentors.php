09b3beefed128bd94f4e8924f8942d61476e55c5
Corey McKrill <corey.mckrill@gmail.com>
2016-12-04 13:22:55 -0500

    Initial commit

---
+++ wordcamp-mentors.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Wordcamp Mentors
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wordcamp-mentors
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wordcamp_Mentors
+ */
