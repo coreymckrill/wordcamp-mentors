coreymckrill/wordcamp-mentors

commit 2e1eafd3b907c813c27447d656f3abc1a6f9b7f0

Print initial task data in page source rather than fetching via Ajax
Having the data in the HTML source makes the application load faster
and avoids making the user wait and watch a spinner while the page
makes an additional Ajax call.

js/tasks/views.js
@@ -43,30 +43,22 @@
 			var view = this,
 				origContent = this.$el.html();
 
-			this.$el
-				.addClass( 'loading-content' )
-				.html( '<tr><td colspan="4"><span class="spinner"></span></td></tr>' );
-
 			this.setLastActive();
 
-			this.tasks      = new wp.api.collections.Wcm_task();
-			this.categories = new wp.api.collections.Wcm_task_category();
+			this.tasks      = new wp.api.collections.Wcm_task( WordCampMentorsTaskData );
+			this.categories = new wp.api.collections.Wcm_task_category( WordCampMentorsTaskCategoryData );
 			this.filter     = new wordcamp.mentors.views.Filter( { el: '#tasks-filter', list: this } );
 			this.reset      = new wordcamp.mentors.views.Reset( { el: '#tasks-reset' } );
 
-			$.when( this.tasks.fetch( this.taskRequest ), this.categories.fetch( this.categoryRequest ) ).done( function() {
-				view.$el.removeClass( 'loading-content' );
+			if ( this.tasks.length ) {
+				view.render();
 
-				if ( view.tasks.length ) {
-					view.render();
-
-					view.ticker = setInterval( function() {
-						view.trigger( 'tick:' + view.tick );
-					}, view.tick );
-				} else {
-					view.$el.html( origContent );
-				}
-			});
+				view.ticker = setInterval( function() {
+					view.trigger( 'tick:' + view.tick );
+				}, view.tick );
+			} else {
+				view.$el.html( origContent );
+			}
 
 			this.listeners();
 		},

includes/tasks-list.php
@@ -154,9 +154,7 @@ class List_Table extends \WP_List_Table {
 	 *
 	 * @since 1.0.0
 	 */
-	public function no_items() {
-		esc_html_e( 'No tasks found.', 'wordcamp-mentors' );
-	}
+	public function no_items() {}
 
 	/**
 	 * Specify the column names and order

includes/tasks.php
@@ -300,6 +300,29 @@ function print_templates() {
 		<?php $js_list_table->single_row_columns( $js_list_table->items[0] ); ?>
 	</script>
 <?php
+	// Initial data
+	$request_url = add_query_arg( array(
+		'per_page' => 300,
+		'orderby'  => 'menu_order',
+		'order'    => 'asc',
+	), get_rest_url( null, 'wp/v2/' . Mentors\PREFIX . '_task' ) );
+
+	$initial_tasks = rest_do_request( \WP_REST_Request::from_url( $request_url ) );
+
+	$request_url = add_query_arg( array(
+		'per_page' => 100,
+	), get_rest_url( null, 'wp/v2/' . Mentors\PREFIX . '_task_category' ) );
+
+	$initial_task_categories = rest_do_request( \WP_REST_Request::from_url( $request_url ) );
+	?>
+	<script type="text/javascript">
+		/* <![CDATA[ */
+		var WordCampMentorsTaskData = <?php echo wp_json_encode( $initial_tasks->data ); ?>;
+		var WordCampMentorsTaskCategoryData = <?php echo wp_json_encode( $initial_task_categories->data ); ?>;
+
+		/* ]]> */
+	</script>
+<?php
 }
 
 add_action( 'admin_print_footer_scripts-dashboard_page_' . Mentors\PREFIX . '-planning-checklist', __NAMESPACE__ . '\print_templates' );

commit 19f7f7b87f124273827c81a39736b5fc9b8ff1f6

Fix list filtering on initial load
The filter listener that fires an update to the visibility of tasks
wasn't getting initialized until after the filtering event had already
been triggered.

This was a regression introduced in 2e1eafd

js/tasks/views.js
@@ -59,6 +59,8 @@
 			this.filter     = new wordcamp.mentors.views.Filter( { el: '#tasks-filter', list: this } );
 			this.reset      = new wordcamp.mentors.views.Reset( { el: '#tasks-reset' } );
 
+			this.listeners();
+
 			if ( this.tasks.length ) {
 				view.render();
 
@@ -66,8 +68,6 @@
 					view.trigger( 'tick:' + view.tick );
 				}, view.tick );
 			}
-
-			this.listeners();
 
 			return this;
 		},
@@ -276,9 +276,9 @@
 			this.list       = options.list;
 			this.categories = options.categories;
 
-			this.render( this._compileData( this.model ) );
-
 			this.listeners();
+
+			this.render( this._compileData( this.model ) );
 
 			return this;
 		},