e867ac783ebcb83cb0d2fca40fbc2cf948bedce5
Corey McKrill <corey.mckrill@gmail.com>
2017-03-23 10:09:51 -0700

    Combine task JS into one file
    Originally I thought there would be multiple JS files, so I set up
    one file to load the other ones asynchronously. However, there ended
    up being only one additional file, so it made more sense to just
    have everything in the one file. This commit removes the loader and
    gives the main file the name that the loader had before.

--- js/tasks/dashboard.js
+++ js/tasks/dashboard.js
@@ -1,4 +1,4 @@
-/*global jQuery, wp, wordcamp, WordCampMentors */
+/*global jQuery, Backbone, _, wp, wordcamp, WordCampMentors, WordCampMentorsTaskData, WordCampMentorsTaskCategoryData, setUserSetting */
 
 ( function( window, $ ) {
 
@@ -6,55 +6,491 @@
 
 	window.wordcamp = window.wordcamp || {};
 
-	wordcamp.MentorsApp = function() {
-		this.cache = {};
-		this.views = {};
-	};
+	wordcamp.mentors = $.extend( {
+		views: {}
+	}, WordCampMentors );
 
-	wordcamp.mentors = new wordcamp.MentorsApp();
+	var $document = $( document ),
+		prefix = wordcamp.mentors.prefix;
 
-	var App = $.extend( wordcamp.mentors, WordCampMentors, {
+	/**
+	 * A Backbone view for the list of tasks.
+	 */
+	wordcamp.mentors.views.List = Backbone.View.extend( {
 		/**
-		 * Load scripts and then kick off the app.
-		 *
-		 * The scripts will populate properties of the App object, such as views.
+		 * Time increment in ms for polling the server.
 		 */
-		load: function() {
-			App.getMultipleScripts( App.scripts ).done( App.init );
+		tick: 5000,
+
+		/**
+		 * Unix timestamp of the last activity within the app.
+		 */
+		lastActive: 0,
+
+		/**
+		 *
+		 */
+		hibernating: false,
+
+		/**
+		 * Data to submit when fetching a task collection.
+		 */
+		taskRequest: {
+			data: {
+				per_page: 300,
+				orderby: 'menu_order',
+				order: 'asc'
+			},
+			remove: false
 		},
 
 		/**
-		 * Initialize the app.
+		 * Initialize the List view.
+		 *
+		 * @returns {wordcamp.mentors.views.List}
 		 */
-		init: function() {
-			var cache = App.cache;
+		initialize: function() {
+			var view = this;
 
-			cache.list = new wordcamp.mentors.views.List( { el: '#the-list' } );
+			this.setLastActive();
+
+			this.tasks      = new wp.api.collections.Wcm_task( WordCampMentorsTaskData );
+			this.categories = new wp.api.collections.Wcm_task_category( WordCampMentorsTaskCategoryData );
+			this.filter     = new wordcamp.mentors.views.Filter( { el: '#tasks-filter', list: this } );
+			this.reset      = new wordcamp.mentors.views.Reset( { el: '#tasks-reset' } );
+
+			this.listeners();
+
+			if ( this.tasks.length ) {
+				view.render();
+
+				view.ticker = setInterval( function() {
+					view.trigger( 'tick:' + view.tick );
+				}, view.tick );
+			}
+
+			return this;
 		},
 
 		/**
-		 * Load an array of scripts using promises so a callback can be
-		 * used when all scripts have finished loading.
+		 * Render the List view.
 		 *
-		 * @link https://stackoverflow.com/a/11803418
-		 *
-		 * @param paths
-		 * @returns {*|$.Deferred.promise}
+		 * @returns {wordcamp.mentors.views.List}
 		 */
-		getMultipleScripts: function( paths ) {
-			var _arr = $.map( paths, function( path ) {
-					return $.getScript( path );
+		render: function() {
+			var view = this;
+
+			this.$el.empty();
+
+			this.tasks.each( function( model ) {
+				var categories, task;
+
+				categories = _.filter( view.categories.models, function( category ) {
+					return _.contains( model.get( prefix + '_task_category' ), category.get( 'id' ) );
 				});
 
-			_arr.push( $.Deferred( function( deferred ) {
-				$( deferred.resolve );
-			}));
+				task = new wordcamp.mentors.views.Task( {
+					model: model,
+					list: view,
+					categories: categories
+				});
 
-			return $.when.apply( $, _arr );
+				view.$el.append( task.$el );
+			});
+
+			this.trigger( 'setFilter', { skipHighlight: true } );
+
+			return this;
+		},
+
+		/**
+		 * Set up event listeners.
+		 *
+		 * @returns {wordcamp.mentors.views.List}
+		 */
+		listeners: function() {
+			this.listenTo( this, 'tick:' + this.tick,   this.pollCollectionOrHibernate );
+			this.listenTo( this, 'hibernate',           this.hibernate );
+			this.listenTo( this.filter, 'filter:tasks', this.updateVisibleTasks );
+
+			return this;
+		},
+
+		/**
+		 * Poll the task collection for changes, or stop polling if the user is inactive.
+		 *
+		 * @returns {wordcamp.mentors.views.List}
+		 */
+		pollCollectionOrHibernate: function() {
+			var elapsed = Date.now() - this.lastActive;
+
+			if ( elapsed < 30000 ) {
+				this.tasks.fetch( this.taskRequest );
+			} else if ( ! this.hibernating ) {
+				this.trigger( 'hibernate' );
+			}
+
+			return this;
+		},
+
+		/**
+		 * Get an array of models that should be visible based on filter parameters.
+		 *
+		 * @private
+		 *
+		 * @param {object} filter
+		 *
+		 * @returns array
+		 */
+		_getVisibleTasks: function( filter ) {
+			var visibleTasks = new Backbone.Collection( this.tasks.models );
+
+			if ( 'any' !== filter[ prefix + '_task_category' ] ) {
+				visibleTasks = new Backbone.Collection( _.filter( visibleTasks.models, function( task ) {
+					return _.contains( task.get( prefix + '_task_category' ), parseInt( filter[ prefix + '_task_category' ], 10 ) );
+				}) );
+			}
+
+			if ( 'any' !== filter.status ) {
+				visibleTasks = new Backbone.Collection( visibleTasks.where( { status: filter.status } ) );
+			}
+
+			return visibleTasks.models;
+		},
+
+		/**
+		 * Update the visibility of tasks in the list based on filter parameters.
+		 *
+		 * @param {object} filter Required parameters to determine which tasks should be visible.
+		 * @param {object} data   Optional parameters to pass to the event trigger.
+		 * @returns {wordcamp.mentors.views.List}
+		 */
+		updateVisibleTasks: function( filter, data ) {
+			var visibleTasks = this._getVisibleTasks( filter ),
+				$parentTable = this.$el.parents( 'table' );
+
+			// Remove row stripes if not showing everything
+			if ( _.every( filter, function( value ) { return 'any' === value; } ) ) {
+				$parentTable.addClass( 'striped' );
+			} else {
+				$parentTable.removeClass( 'striped' );
+			}
+
+			this.tasks.each( function( task ) {
+				if ( ! _.contains( visibleTasks, task ) ) {
+					task.trigger( 'visibility', 'hide', data );
+				}
+			});
+
+			_.each( visibleTasks, function( task ) {
+				task.trigger( 'visibility', 'show', data );
+			});
+
+			return this;
+		},
+
+		/**
+		 * Set or update the application as active so that it will poll for changes.
+		 *
+		 * @returns {wordcamp.mentors.views.List}
+		 */
+		setLastActive: function() {
+			this.lastActive  = Date.now();
+			this.hibernating = false;
+
+			// Stop listening for activity
+			$document.off( '.' + prefix + '-tasks' );
+
+			return this;
+		},
+
+		/**
+		 * Set the application as inactive so that it won't poll for changes.
+		 *
+		 * @returns {wordcamp.mentors.views.List}
+		 */
+		hibernate: function() {
+			var view = this;
+
+			this.hibernating = true;
+
+			$document.on(
+				'mouseover.' + prefix + '-tasks keyup.' + prefix + '-tasks touchend.' + prefix + '-tasks',
+				function() {
+					view.setLastActive();
+				}
+			);
+
+			return this;
+		}
+	});
+
+	/**
+	 * A Backbone view for an individual task.
+	 */
+	wordcamp.mentors.views.Task = Backbone.View.extend( {
+		/**
+		 * HTML element to use as a container.
+		 */
+		tagName: 'tr',
+
+		/**
+		 * HTML element ID attribute.
+		 *
+		 * @returns {string}
+		 */
+		id: function() {
+			return prefix + '-task-' + this.model.get( 'id' );
+		},
+
+		/**
+		 * HTML element class attribute.
+		 */
+		className: prefix + '-task',
+
+		/**
+		 * The templating function for rendering the task.
+		 */
+		template: wp.template( prefix + '-task' ),
+
+		/**
+		 * Combine model attributes with other data necessary for rendering.
+		 *
+		 * @private
+		 *
+		 * @param {wp.api.models.Wcm_task} model
+		 *
+		 * @returns {object}
+		 */
+		_compileData: function( model ) {
+			return $.extend( {}, model.attributes, {
+				task_category: this.categories,
+				stati: wordcamp.mentors.stati
+			} );
+		},
+
+		/**
+		 * Initialize a task view.
+		 *
+		 * @param {object} options
+		 *
+		 * @returns {wordcamp.mentors.views.Task}
+		 */
+		initialize: function( options ) {
+			this.list       = options.list;
+			this.categories = options.categories;
+
+			this.listeners();
+
+			this.render( this._compileData( this.model ) );
+
+			return this;
+		},
+
+		/**
+		 * Render a task.
+		 *
+		 * @param {object} data
+		 *
+		 * @returns {wordcamp.mentors.views.Task}
+		 */
+		render: function( data ) {
+			this.$el.html( this.template( data ) );
+
+			return this;
+		},
+
+		/**
+		 * Set up event listeners.
+		 *
+		 * @returns {wordcamp.mentors.views.Task}
+		 */
+		listeners: function() {
+			this.listenTo( this.model, 'visibility',      this.changeVisibility );
+			this.listenTo( this.model, 'change:status',   this.changeStatus );
+			this.listenTo( this.model, 'change:modified', this.changeModified );
+
+			return this;
+		},
+
+		/**
+		 * Change the visibility of this view's element.
+		 *
+		 * @param {string} action
+		 * @param {object} options
+		 *
+		 * @returns {wordcamp.mentors.views.Task}
+		 */
+		changeVisibility: function( action, options ) {
+			options = _.defaults( options || {}, {
+				skipHighLight: false
+			} );
+
+			var duration = ( options.skipHighLight ) ? 0 : 500;
+
+			if ( false === options.skipHighLight ) {
+				this.$el.addClass( prefix + '-highlight' );
+			}
+
+			switch ( action ) {
+				case 'show' :
+					this.$el.fadeIn( duration );
+					break;
+
+				case 'hide' :
+					this.$el.fadeOut( duration );
+					break;
+			}
+
+			this.$el.promise().done( function() {
+				$( this ).removeClass( prefix + '-highlight' );
+			});
+
+			return this;
+		},
+
+		/**
+		 * Re-render this task when the status changes.
+		 *
+		 * @param model
+		 */
+		changeStatus: function( model ) {
+			var list = this.list;
+
+			this.$el.addClass( prefix + '-highlight' );
+
+			this.render( this._compileData( model ) );
+
+			// Slight delay before re-filtering the list
+			setTimeout( function() {
+				list.trigger( 'setFilter' );
+			}, 1000 );
+		},
+
+		/**
+		 * Re-render this task when the modified timestamp changes.
+		 *
+		 * @param {wp.api.models.Wcm_task} model
+		 */
+		changeModified: function( model ) {
+			this.render( this._compileData( model ) );
+		},
+
+		/**
+		 * Event binding.
+		 */
+		events: {
+			'change .column-status select': 'updateStatus'
+		},
+
+		/**
+		 * Update this task's model when the user chooses a new status in the UI.
+		 *
+		 * @param {event} event
+		 *
+		 * @returns {wordcamp.mentors.views.Task}
+		 */
+		updateStatus: function( event ) {
+			var value = $( event.target ).val();
+
+			this.model.set( 'status', value );
+			this.model.save();
+
+			return this;
+		}
+	});
+
+	/**
+	 * A Backbone view for the controls that filter visible tasks.
+	 */
+	wordcamp.mentors.views.Filter = Backbone.View.extend( {
+		/**
+		 * Initialize the filter view.
+		 *
+		 * @param {object} options
+		 *
+		 * @returns {wordcamp.mentors.views.Filter}
+		 */
+		initialize: function( options ) {
+			this.list = options.list;
+
+			this.listeners();
+
+			return this;
+		},
+
+		/**
+		 * Set up event listeners.
+		 *
+		 * @returns {wordcamp.mentors.views.Filter}
+		 */
+		listeners: function() {
+			this.listenTo( this.list, 'setFilter', function( data ) {
+				this.$el.trigger( 'submit', data );
+			} );
+
+			return this;
+		},
+
+		/**
+		 * Event binding.
+		 */
+		events: {
+			'submit': 'setFilter'
+		},
+
+		/**
+		 * Gather the parameters set for the list filter and pass them via event trigger.
+		 *
+		 * @param {event}  event
+		 * @param {object} data
+		 *
+		 * @returns {wordcamp.mentors.views.Filter}
+		 */
+		setFilter: function( event, data ) {
+			event.preventDefault();
+
+			var filter = {},
+				settingPrefix = wordcamp.mentors.prefix;
+
+			$( event.target ).find( 'select' ).each( function() {
+				var attribute = $( this ).data( 'attribute' );
+
+				filter[ attribute ] = $( this ).val();
+			});
+
+			// Save filter values as user settings
+			_.each( filter, function( value, key ) {
+				setUserSetting(
+					settingPrefix + '-' + key,
+					value
+				);
+			});
+
+			this.trigger( 'filter:tasks', filter, data );
+
+			return this;
+		}
+	});
+
+	/**
+	 * A Backbone view for the button to reset task data.
+	 */
+	wordcamp.mentors.views.Reset = Backbone.View.extend( {
+		events: {
+			'submit': 'confirm'
+		},
+
+		confirm: function( event ) {
+			if ( ! window.confirm( wordcamp.mentors.l10n.confirmReset ) ) {
+				event.preventDefault();
+			}
 		}
 	});
 
 	// Ensure the Backbone client is loaded before getting started.
-	wp.api.loadPromise.done( App.load );
+	wp.api.loadPromise.done( function () {
+		wordcamp.mentors.list = new wordcamp.mentors.views.List( { el: '#the-list' } );
+	});
 
 } )( window, jQuery );
