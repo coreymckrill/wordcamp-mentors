35891fbe402a0f554fa6ac14bf903cd7074a7f44
Corey McKrill <corey.mckrill@gmail.com>
2017-04-07 15:56:03 -0700

    Add spacing and icons to task links

--- includes/tasks.php
+++ includes/tasks.php
@@ -372,8 +372,11 @@ function print_templates() {
 		<td class="task column-task">
 			{{ data.excerpt.rendered }}
 			<# if ( data.helpLink.text && data.helpLink.url ) { #>
-				<br />
-				<a href="{{ data.helpLink.url }}" target="_blank" class="<?php echo esc_attr( Mentors\PREFIX ); ?>-help-link">{{ data.helpLink.text }}</a>
+				<br /><br />
+				<a href="{{ data.helpLink.url }}" target="_blank" class="<?php echo esc_attr( Mentors\PREFIX ); ?>-help-link">
+					{{ data.helpLink.text }}
+					<span class="dashicons dashicons-external" aria-hidden="true"></span>
+				</a>
 			<# } #>
 		</td>
 		<td class="" colspan="<?php echo esc_attr( $columns - 1 ); ?>">
